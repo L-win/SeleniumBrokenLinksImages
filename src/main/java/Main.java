b3644dfb3e8dd2aee5a80fb94976e1852497713c
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-05-09 11:00:09 +0400

    Use Selenium via Maven.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,14 @@
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import tests.BrokenLinks;
 import tests.BrokenImages;
 
 public class Main {
 
-	private static final String PATH_CHROME = "drivers\\chromedriver-113.exe";
-	private static final String PATH_FIREFOX = "drivers\\geckodriver-0.33.0.exe";
-	private static final String PATH_EDGE = "drivers\\msedgedriver-112.exe";
-
-	@BeforeSuite
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", PATH_CHROME);
-		System.setProperty("webdriver.edge.driver", PATH_EDGE);
-		System.setProperty("webdriver.firefox.driver", PATH_FIREFOX);
-	}
-
-	@Test(priority = 1)
+	// @Test(priority = 1)
 	private static void links(){
 		WebDriver driver = new ChromeDriver();
 		String host = "https://google.com";
@@ -32,7 +20,7 @@ public class Main {
 	@Test(priority = 2)
 	private static void	images(){
 		WebDriver driver = new ChromeDriver();
-		host = "https://en.wikipedia.org/wiki/Main_Page";
+		String host = "https://en.wikipedia.org/wiki/Main_Page";
 		BrokenImages.findImageLinks(driver,host);
 	}
 
